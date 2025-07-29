5f5510cf9e5b05aa1087a508f9d9c57a04925901
Sheikh Haris Zahid <itshariszahid@gmail.com>
2025-07-29 18:37:48 +0500

    feat: Configured font and color pallete

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-full h-screen bg-green-300 flex items-center justify-center">
-      <h1 className="font-bold text-5xl">Tailwind Configured</h1>
+      <h1 className="text-5xl font-Inter">Inter</h1>
     </div>
   );
 }

547a6456fb1c6ebcefb53b5701098ef470988037
Sheikh Haris Zahid <itshariszahid@gmail.com>
2025-08-05 14:16:05 +0500

    feat: Forgot password screen

---
+++ apps/web/app/(authentication)/forgot-password/page.tsx
@@ -0,0 +1,34 @@
+import Button from '@/components/Button';
+import Heading from '@/components/Heading';
+import Input from '@/components/Input';
+import Paragraph from '@/components/Paragraph';
+import React from 'react';
+
+const ForgotPasswordPage = () => {
+  return (
+    <>
+      <Heading
+        text="Forgot Your Password?"
+        className="text-center mb-1 2xl:mb-2"
+      />
+      <Paragraph
+        text={`No worries! Enter your email address and we'll send you instructions to reset your password.`}
+        mute
+        className="text-center max-w-[85vw] xs:max-w-4/5 sm:max-w-2/3 mx-auto mb-10 md:mb-16 xl:mb-20 2xl:mb-24"
+      />
+      <form
+        action=""
+        className="min-w-[90vw] xs:min-w-[300px] md:min-w-[340px] xl:min-w-sm mx-auto space-y-8 md:space-y-12"
+      >
+        <Input type="email" label="Email Address" id="email" />
+        <Button
+          type="submit"
+          text=" Send Reset Link"
+          className="w-3/5 mx-auto"
+        />
+      </form>
+    </>
+  );
+};
+
+export default ForgotPasswordPage;

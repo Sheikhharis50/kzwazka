3f49106947199ffced98bf0f8cbd345922440ad9
Sheikh Haris Zahid <itshariszahid@gmail.com>
2025-08-12 12:13:32 +0500

    feat: added the crud entry points

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ChildrenModule } from './children/children.module';
+import { LocationModule } from './location/location.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ChildrenModule } from './children/children.module';
     AuthModule,
     UserModule,
     ChildrenModule,
+    LocationModule,
   ],
   controllers: [AppController],
   providers: [AppService],

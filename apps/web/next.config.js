7a7106eda132354035113d885e55619688fb1015
Sheikh Haris Zahid <itshariszahid@gmail.com>
2025-09-10 17:59:29 +0500

    fix: Fixed photo url

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kzwazka.fra1.cdn.digitaloceanspaces.com',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
-        source: "/api/:path*",   // when you call /api/*
+        source: '/api/:path*', // when you call /api/*
         // eslint-disable-next-line no-undef
         destination: `http://localhost:${process.env.SERVER_PORT}/api/:path*`, // forward to your NestJS server
       },
